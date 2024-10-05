0e0e57debff9eeed3d69a7ea5bf374f3557d3578
facetint <fatmanurcetintas7@gmail.com>
2024-10-05 23:44:15 +0300

    feat: add setter method and zombie message

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -9,3 +9,13 @@ Zombie::~Zombie()
 {
     std::cout << this->_name << "Zombie destroyed" << std::endl;
 }
+
+void Zombie::setName(std::string name)
+{
+    this->_name = name;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << "<" << this->_name << ">: BraiiiiiiinnnzzzZ..." << std::endl;
+}
