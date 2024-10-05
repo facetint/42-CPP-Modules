c6b7b6cacf14684b984348608da01bc541d51dab
facetint <fatmanurcetintas7@gmail.com>
2024-10-05 22:04:01 +0300

    feat: add variables that are private for the Contact class

---
+++ ex01/Contact.hpp
@@ -0,0 +1,17 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+#include <iostream>
+
+class Contact
+{
+    private:
+    std::string     _name;
+    std::string     _lastName;
+    std::string     _nickName;
+    std::string     _phoneNumber;
+    std::string     _darkestSecret;
+};
+
+#endif
