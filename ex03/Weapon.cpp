5e10802e2e83b1afa7f5ab11fe34fe64753367d9
Fatmanur Cetintas <facetint@k1m22s01.42kocaeli.com.tr>
2024-10-08 20:13:51 +0300

    feat: handle the constructor, destructor, get type and set type functions of the Weapon class

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 05:41:00 by facetint          #+#    #+#             */
+/*   Updated: 2024/10/07 16:17:27 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type)
+{
+    this->_type = type;
+}
+Weapon::~Weapon(void)
+{
+    std::cout << "Weapon Destroyed" << std::endl;
+}
+
+const std::string &Weapon::getType(void) const
+{
+    return (this->_type);
+}
+
+void Weapon::setType(std::string type)
+{
+    this->_type = type;
+}
