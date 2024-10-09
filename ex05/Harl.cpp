5fd20550db72d4175c891b58cefa213b82f9df41
Fatmanur Cetintas <facetint@k1m22s01.42kocaeli.com.tr>
2024-10-09 12:20:06 +0300

    feat: print messages from debug, info, warning and error methods

---
+++ ex05/Harl.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 14:55:13 by facetint          #+#    #+#             */
+/*   Updated: 2024/10/09 12:18:04 by facetint         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {
+
+    std::cout << "Harl is here!" << std::endl;
+}
+
+Harl::~Harl() {
+
+    std::cout << "Harl is leaving!" << std::endl;
+}
+
+void Harl::debug(void) {
+    std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout << "INFO: I can't believe adding extra bacon cost more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
+}
