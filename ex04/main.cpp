c52500782350588cdcc1ba7a0f6850a5dbb92f2d
Fatmanur Cetintas <facetint@k1m22s08.42kocaeli.com.tr>
2024-10-10 22:46:56 +0300

    fix: solved the infinite loop problem if arguments 2 and 3 are given the same or have the same head but different sides

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,64 +6,59 @@
 /*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 19:46:33 by facetint          #+#    #+#             */
-/*   Updated: 2024/10/10 20:00:32 by facetint         ###   ########.fr       */
+/*   Updated: 2024/10/10 22:44:52 by facetint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 std::string ft_replace(std::string src, std::string find, std::string rep)
 {
     size_t pos = src.find(find);
-    std::string ret_val;
+    std::string result;
+
+    size_t lastPos = 0;
 
     while (pos != std::string::npos)
     {
-        ret_val = src.substr(0, pos);
-        ret_val += rep;
-        ret_val += src.substr(pos + find.length(), src.length() - (pos + find.length()));
-        src = ret_val;
-        pos = src.find(find);
+        result += src.substr(lastPos, pos - lastPos);
+        result += rep;
+        lastPos = pos + find.length();
+        pos = src.find(find, lastPos);
     }
-    if (ret_val.length() == 0 && ret_val.empty())
-        return (src);
-    return (ret_val);
+    result += src.substr(lastPos);
+    return result;
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    if (ac == 4)
-    {
-        std::string s1 = av[2];
-        std::ifstream in_file(av[1]);
-        if (!in_file)
-        {
-            std::cout << "File not found" << std::endl;
-            return 1;
-        }
-        std::string s2 = av[3];
-        if (s1 == s2)
-        {
-            std::cout << "The strings are the same" << std::endl;
-            return 1;
-        }
-        std::string file = av[1];
-        std::string line;
-        std::ofstream output_file(file + ".replace");
-        if (!in_file)
-        {
-            std::cout << "File not found" << std::endl;
-            return 1;
-        }
-        while (std::getline(in_file, line))
-        {
-            line = ft_replace(line, s1, s2);
-            output_file << line << std::endl;
-        }
-        in_file.close();
-        output_file.close();
-    }
-    else
-        std::cout << "Wrong Arguments" << std::endl;
+	if (ac == 4)
+	{
+		std::string s1 = av[2];
+		std::ifstream in_file(av[1]);
+		if (!in_file)
+		{
+			std::cout << "File not found" << std::endl;
+			return (1);
+		}
+		std::string s2 = av[3];
+		std::string file = av[1];
+		std::string line;
+		std::ofstream output_file(file + ".replace");
+		if (!in_file)
+		{
+			std::cout << "File not found" << std::endl;
+			return (1);
+		}
+		while (std::getline(in_file, line))
+		{
+			line = ft_replace(line, s1, s2);
+			output_file << line << std::endl;
+		}
+		in_file.close();
+		output_file.close();
+	}
+	else
+		std::cout << "Wrong Arguments" << std::endl;
 }
