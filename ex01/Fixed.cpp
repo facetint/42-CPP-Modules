b0052ad3a360b977454d6f0a1bd26dac9d8308d5
facetint <fatmanurcetintas7@gmail.com>
2024-10-13 02:18:38 +0300

    feat: add parameterized, parameterless and copy constructor, and set destructor

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -4,3 +4,26 @@ Fixed::Fixed(void){
     std::cout << "Default constructor called" << std::endl;
     this->value = 0;
 }
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::Fixed(const int number)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->value = number << this->bitCount;
+}
+
+Fixed::Fixed(const float number)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->value = roundf(number * (1 << this->bitCount));
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
