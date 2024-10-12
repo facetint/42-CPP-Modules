c11937024a8b443a211d4d2e84d57804b6b80201
facetint <fatmanurcetintas7@gmail.com>
2024-10-12 20:08:37 +0300

    style: make space arrangement

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,12 @@
 
 int main(void) {
 	Fixed a;
-	Fixed b( a );
+	Fixed b(a);
 	Fixed c;
 	c = b;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
-
 	return 0;
 }
