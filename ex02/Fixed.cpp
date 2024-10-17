1de8ab7b5a9cba3eae3f133f1cafd309a08697e1
Fatmanur Cetintas <facetint@k2m08s09.42kocaeli.com.tr>
2024-10-17 15:46:27 +0300

    feat: create a copy constructor

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -8,3 +8,8 @@ Fixed::Fixed(){
 Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
 }
+
+Fixed::Fixed(const Fixed& other){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
