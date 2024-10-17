d2a0c76fa8aeff31f2dfc3f5cca77e1e91efcbfa
Fatmanur Cetintas <facetint@k2m08s09.42kocaeli.com.tr>
2024-10-17 16:11:18 +0300

    refactor: replace the destructor method

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -5,10 +5,6 @@ Fixed::Fixed(){
 	value = 8;
 }
 
-Fixed::~Fixed(){
-	std::cout << "Destructor called" << std::endl;
-}
-
 Fixed::Fixed(const Fixed& other){
 	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
@@ -20,4 +16,8 @@ Fixed::Fixed(const int number)
 }
 Fixed::Fixed(const float number){
 	this->value = roundf(number * (1 << this->bitCount));
+}
+
+Fixed::~Fixed(){
+	std::cout << "Destructor called" << std::endl;
 }
