e4af54667da9b418ef988d997741960b05ab494c
Fatmanur Cetintas <facetint@k2m10s02.42kocaeli.com.tr>
2024-10-23 14:04:33 +0300

    feat: create a parameterized constructor

--- ClapTrap.cpp
+++ ClapTrap.cpp
@@ -4,3 +4,9 @@ ClasTrap::ClasTrap()
 {
     std::cout << "Default Constructor called" << std::endl; 
 }
+ClasTrap::ClasTrap(std::string name)
+{
+    this->hitPoint = 10;
+    this->energyPoint = 10;
+    this->attackDamage = 0;
+}
