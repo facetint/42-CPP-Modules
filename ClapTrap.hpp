64621a93b96811f3085d062e0037417a4938d30c
Fatmanur Cetintas <facetint@k2m10s02.42kocaeli.com.tr>
2024-10-23 14:09:05 +0300

    fix: fixed the problem of misspelling the prototype of a member function

--- ClapTrap.hpp
+++ ClapTrap.hpp
@@ -16,7 +16,7 @@ private:
 public:
     ClasTrap(std::string name);
     void attack(const std::string& target);
-    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
 };
 
