25f5a41dd76cbca8fd08edd7a49110e06868633f
Fatmanur Cetintas <facetint@k2m10s08.42kocaeli.com.tr>
2024-10-23 14:53:00 +0300

    fix: fix typo in ClasTrap class methods and implement getter/setter functions

--- ClapTrap.cpp
+++ ClapTrap.cpp
@@ -1,21 +1,54 @@
 #include "ClapTrap.hpp"
 
-ClasTrap::ClasTrap()
+ClapTrap::ClapTrap()
 {
     std::cout << "Default Constructor called" << std::endl; 
 }
-ClasTrap::ClasTrap(std::string name)
+ClapTrap::ClapTrap(std::string name)
 {
     this->hitPoint = 10;
     this->energyPoint = 10;
     this->attackDamage = 0;
 }
-ClasTrap::ClasTrap(const ClasTrap& other)
+ClapTrap::ClapTrap(const ClapTrap& other)
 {
     *this = other;
     std::cout << "Copy constructor called" << std::endl;
 }
-void ClasTrap::attack(const std::string& target)
+unsigned int ClapTrap::getAttackDamage() const
+{
+    return this->attackDamage;
+}
+
+unsigned int ClapTrap::getEnergyPoint() const
+{
+    return this->energyPoint;
+}
+
+unsigned int ClapTrap::getHitPoint() const
+{
+    return this->hitPoint;
+}
+
+void ClapTrap::setAttackDamage(unsigned int attackDamage)
+{
+    this->attackDamage = attackDamage;
+}
+
+void ClapTrap::setEnergyPoint(unsigned int energyPoint)
+{
+    this->energyPoint = energyPoint;
+}
+void ClapTrap::setHitPoint(unsigned int hitPoint)
+{
+    this->hitPoint = hitPoint;
+}
+void ClapTrap::setName(std::string name)
+{
+    this->name = name;
+}
+
+void ClapTrap::attack(const std::string& target)
 {
    std::cout << "ClapTrap " << this->name <<  " attacks " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
 }
