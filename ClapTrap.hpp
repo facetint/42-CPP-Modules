8f3866db77450df3ef5a3186984525f45edad8a2
Fatmanur Cetintas <facetint@k2m10s08.42kocaeli.com.tr>
2024-10-23 14:43:48 +0300

    feat: add getter and setter methods for hitPoint, energyPoint, attackDamage, and name in ClasTrap

--- ClapTrap.hpp
+++ ClapTrap.hpp
@@ -21,6 +21,15 @@ public:
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
     ClasTrap& operator=(const ClasTrap& other);
+    unsigned int getHitPoint() const;
+    unsigned int getEnergyPoint() const;
+    unsigned int getAttackDamage() const;
+    std::string getName() const;
+    void setName(std::string name);
+    void setHitPoint(unsigned int hitPoint);
+    void setEnergyPoint(unsigned int energyPoint);
+    void setAttackDamage(unsigned int attackDamage);
+    
 };
 
 #endif
