1c162756da11ebaee497e859a2cd78ea7355bcc8
Fatmanur Cetintas <facetint@k2m10s08.42kocaeli.com.tr>
2024-10-23 14:41:22 +0300

    feat: add copy assignment operator and destructor to ClasTrap class

--- ClapTrap.hpp
+++ ClapTrap.hpp
@@ -16,9 +16,11 @@ private:
 public:
     ClasTrap(std::string name);
     ClasTrap(const ClasTrap& other);
+    ~ClasTrap();
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
+    ClasTrap& operator=(const ClasTrap& other);
 };
 
 #endif
