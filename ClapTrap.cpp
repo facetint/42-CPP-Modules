3ded24d5b30a9fda494101adc81d6a18e428d69b
Fatmanur Cetintas <facetint@k2m10s08.42kocaeli.com.tr>
2024-10-23 14:37:42 +0300

    feat: suppress the screen output message of the attcak member function

--- ClapTrap.cpp
+++ ClapTrap.cpp
@@ -15,3 +15,8 @@ ClasTrap::ClasTrap(const ClasTrap& other)
     *this = other;
     std::cout << "Copy constructor called" << std::endl;
 }
+void ClasTrap::attack(const std::string& target)
+{
+   std::cout << "ClapTrap " << this->name <<  " attacks " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
+}
+
