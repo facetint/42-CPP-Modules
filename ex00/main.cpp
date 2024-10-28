872b305a15295878e9772408396b9f60e16fecd1
facetint <fatmanurcetintas7@gmail.com>
2024-10-28 23:05:40 +0300

    feat: add main function to test ClapTrap class functionality

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fatmanurcetintas <fatmanurcetintas@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:48:59 by facetint          #+#    #+#             */
-/*   Updated: 2024/10/24 18:01:51 by fatmanurcet      ###   ########.fr       */
+/*   Updated: 2024/10/28 23:04:52 by fatmanurcet      ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,15 +14,26 @@
 
 int main()
 {
-    ClapTrap claptrap("Claptrap");
-    ClapTrap claptrap2("Claptrap2");
+    ClapTrap clap1("Clappy");
 
-    claptrap.attack("Claptrap2");
-    claptrap2.takeDamage(5);
-    claptrap2.beRepaired(5);
-    claptrap.fight(claptrap2);
-    claptrap.displayStats();
-    claptrap2.displayStats();
+    clap1.displayStats();
+    clap1.attack("Target1");
+    clap1.displayStats();
+    clap1.takeDamage(5);
+    clap1.displayStats();
+    clap1.beRepaired(3);
+    clap1.displayStats();
+    
+    for (int i = 0; i < 10; ++i) {
+        clap1.attack("Dummy Target");
+    }
+    clap1.displayStats();
+    clap1.takeDamage(20);
+    clap1.displayStats();
+    std::cout << std::endl;
+
+    clap1.beRepaired(5);
+    clap1.displayStats();
+
     return 0;
-
 }
