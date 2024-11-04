3bc6dc2d3cad46faad214aa266073e6e8096e9f9
facetint <fatmanurcetintas7@gmail.com>
2024-11-04 16:49:17 +0300

    fix: add index range checks in getIdea and setIdea methods of Brain class

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -23,16 +23,26 @@ Brain &Brain::operator=(const Brain &other)
     if (this == &other)
         return *this;
     for (int i = 0; i < 100; i++)
-        ideas[i] = other.getIdea(i);
+            ideas[i] = other.getIdea(i);
     return *this;
 }
 
 std::string Brain::getIdea(int index) const
 {
+    if (index < 0 || index >= 100)
+    {
+        std::cerr << "Index out of range" << std::endl;
+        return ;
+    }
     return ideas[index];
 }
 
 void Brain::setIdea(int index, const std::string &idea)
 {
+    if (index < 0 || index >= 100)
+    {
+        std::cerr << "Index out of range" << std::endl;
+        return ;
+    }
     ideas[index] = idea;
 }
