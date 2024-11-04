446692aa40c17cd9f1af68404626843f40d9aea5
facetint <fatmanurcetintas7@gmail.com>
2024-11-04 14:15:54 +0300

    feat: add getter and setter methods for ideas in Brain class

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -11,5 +11,7 @@ class Brain
         ~Brain();
         Brain(const Brain &other);
         Brain &operator=(const Brain &other);
+        std::string getIdea(int index) const;
+        void setIdea(int index, const std::string &idea);
 
 };
