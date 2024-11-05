79b96e409f88f41d83bd3d8b1eddc2c045205fc9
Fatmanur Cetintas <facetint@k1m10s02.42kocaeli.com.tr>
2024-11-05 17:28:55 +0300

    refactor: update the main function to use the AAnimal abstract class on main.cpp in ex01

---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+#include "Brain.hpp"
+#include "AAnimal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+# define MAX_ANIMALS 10
+
+int main() {
+    try {
+        if (MAX_ANIMALS < 2 || MAX_ANIMALS % 2 != 0)
+            throw std::invalid_argument("MAX_ANIMALS must be a multiple of 2 and greater than 1");
+        
+        const AAnimal* animals[MAX_ANIMALS] = {0};
+
+        for (int i = 0; i < MAX_ANIMALS; i++) {
+            if (i % 2 == 0)
+                animals[i] = new Dog();
+            else
+                animals[i] = new Cat();
+        }
+        for (int i = 0; i < MAX_ANIMALS; i++)
+            animals[i]->display();
+        for (int i = 0; i < MAX_ANIMALS; i++)
+            delete animals[i];
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
