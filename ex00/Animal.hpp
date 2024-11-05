647b6a57cf9f998a36bc919a7679a401006fccbc
Fatmanur Cetintas <facetint@k2m04s01.42kocaeli.com.tr>
2024-11-05 12:57:56 +0300

    style: update console messages in Animal class for clarity

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -4,6 +4,13 @@
 
 # include <iostream>
 
+# define RED_COLOR "\x1b[31;01m"
+# define RESET "\x1b[0m"
+# define GREEN_COLOR "\x1b[32;01m"
+# define BLUE_COLOR "\x1b[34;01m"
+# define PURPLE_COLOR "\x1b[38;2;178;58;238;01m"
+# define WHITE_COLOR "\x1b[38;2;255;255;255;01m"
+
 class Animal{
 
 protected:
@@ -20,6 +27,7 @@ public:
     std::string getType(void) const;
 
     virtual void    makeSound(void) const;
+    virtual void display(void) const;
     void    whoAmI(void) const;
 
 };
