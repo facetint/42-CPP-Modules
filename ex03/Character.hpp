9137c09b077965feae1c310117f2755f57bee900
Fatmanur Cetintas <facetint@k1m10s02.42kocaeli.com.tr>
2024-11-05 18:28:03 +0300

    feat: create the Character class implementing the ICharacter interface and define private members for the character's name and an inventory of AMateria pointers

---
+++ ex03/Character.hpp
@@ -0,0 +1,27 @@
+#ifndef CHARACTER_HPP
+
+# define CHARACTER_HPP
+
+# include "AMateria.hpp"
+# include "ICharacter.hpp"
+
+class Character : public ICharacter
+{
+    private:
+        std::string _name;
+        AMateria* _inventory[4];
+    public:
+        Character();
+        Character(std::string name);
+        Character(const Character& other);
+        ~Character();
+        Character& operator=(const Character& other);
+
+        std::string const& getName() const;
+
+        void equip(AMateria* m);
+        void unequip(int idx);
+        void use(int idx, ICharacter& target);
+};
+
+#endif
