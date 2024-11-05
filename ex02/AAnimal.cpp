2f48945c692b5336d528e24520deaf54c2ccaad5
Fatmanur Cetintas <facetint@k1m10s02.42kocaeli.com.tr>
2024-11-05 17:11:36 +0300

    feat: implement constructors, destructors, copy constructors, and assignment operators for all classes

---
+++ ex02/AAnimal.cpp
@@ -0,0 +1,32 @@
+#include "AAnimal.hpp"
+
+AAnimal::AAnimal() : _type("AAnimal")
+{
+    std::cout << "Default constructor called - (AAnimal)" << std::endl;
+}
+
+AAnimal::AAnimal(std::string type) : _type(type)
+{
+    std::cout << "Parametric constructor called - (AAnimal)" << std::endl;
+}
+AAnimal::AAnimal(const AAnimal& other)
+{
+    *this = other;
+    std::cout << "Copy constructor called - (AAnimal)" << std::endl;
+}
+
+AAnimal::~AAnimal()
+{
+    std::cout << "Destructor called - (AAnimal)" << std::endl;
+}
+
+AAnimal& AAnimal::operator=(const AAnimal& other)
+{
+    if (this != &other)
+    {
+        this->_type = other._type;
+    }
+    std::cout << "Assignation operator called - (AAnimal)" << std::endl;
+    return *this;
+}
+
