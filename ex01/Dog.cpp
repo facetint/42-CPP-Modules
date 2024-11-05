302ba84ef4d79b8034973e57cf8944e2b076e6c6
facetint <fatmanurcetintas7@gmail.com>
2024-11-05 23:41:18 +0300

    fix: add memory allocation error handling in Dog class

--- ex01/Dog.cpp
+++ ex01/Dog.cpp
@@ -2,15 +2,31 @@
 
 Dog::Dog()
 {
-    setType("Dog");
-    brain = new Brain();
     std::cout << "Default constructor called - (Dog)" << std::endl;
+    try
+    {
+        setType("Dog");
+        brain = new Brain();
+    }
+    catch (const std::bad_alloc& e)
+    {
+        std::cerr << "Memory Allocation Error: " << e.what() << std::endl;
+        brain = NULL;
+        throw;
+    }
 }
 
 Dog::Dog(const Dog& other)
 {
-    *this = other;
     std::cout << "Copy constructor called - (Dog)" << std::endl;
+    try {
+        this->brain = new Brain(*other.brain);
+        setType(other.getType());
+    } catch (const std::bad_alloc& e) {
+        std::cerr << "Memory Allocation Error: " << e.what() << std::endl;
+        throw;
+    }
+    *this = other;
 }
 
 Dog::~Dog()
@@ -37,7 +53,6 @@ Dog& Dog::operator=(const Dog& other)
     std::cout << "Assignation operator called - (Dog)" << std::endl;
     return *this;
 }
-
 
 void Dog::makeSound() const
 {
