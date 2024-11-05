d06d99a9b17b6048a8c419b46f64dabbe7ab0144
Fatmanur Cetintas <facetint@k2m04s01.42kocaeli.com.tr>
2024-11-05 12:39:45 +0300

    feat: add color codes for console output in header file

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -2,6 +2,13 @@
 
 #include <iostream>
 
+# define RED_COLOR "\x1b[31;01m"
+# define RESET "\x1b[0m"
+# define GREEN_COLOR "\x1b[32;01m"
+# define BLUE_COLOR "\x1b[34;01m"
+# define PURPLE_COLOR "\x1b[38;2;178;58;238;01m"
+# define WHITE_COLOR "\x1b[38;2;255;255;255;01m"
+
 class Brain
 {
     private:
