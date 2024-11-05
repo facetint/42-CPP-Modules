4f6b6ab0d75b021873dd7404924729fbba8bb3df
Fatmanur Cetintas <facetint@k1m10s02.42kocaeli.com.tr>
2024-11-05 17:09:55 +0300

    feat: define abstract class AAnimal with pure virtual methods for makeSound and display

---
+++ ex02/AAnimal.hpp
@@ -0,0 +1,23 @@
+#ifndef AANIMAL_HPP
+
+# define AANIMAL_HPP
+
+# include <iostream>
+
+class AAnimal
+{
+    protected:
+        std::string _type;
+    public:
+        AAnimal();
+        AAnimal(std::string type);
+        AAnimal(const AAnimal& other);
+        virtual ~AAnimal();
+        AAnimal& operator=(const AAnimal& other);
+        void setType(const std::string type);
+        std::string getType(void) const;
+        virtual void makeSound() const = 0;
+        virtual void display() const = 0;
+};
+
+#endif
