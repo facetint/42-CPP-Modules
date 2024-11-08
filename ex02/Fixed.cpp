844e27dd448b1d2a5893525995cb9cdddb31d4ed
facetint <fatmanurcetintas7@gmail.com>
2024-11-08 17:28:34 +0300

    feat: add arithmetic, comparison, and compound assignment operators

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -2,7 +2,7 @@
 
 Fixed::Fixed(){
 	std::cout << "Default constructor called" << std::endl;
-	value = 8;
+	value = 0;
 }
 
 Fixed::Fixed(const Fixed& other){
@@ -45,7 +45,9 @@ void	Fixed::setRawBit(const int raw){
 Fixed& Fixed::operator=(const Fixed& other){
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
+	{
 		this->value = other.getRawBit();
+	}
 	return (*this);
 }
 
@@ -54,3 +56,99 @@ std::ostream& operator<<(std::ostream& os, const Fixed& fixed){
 	return (os);
 }
 
+bool Fixed::operator>(const Fixed& other) const{
+	return this->value > other.value ? true : false;
+}
+
+bool Fixed::operator<(const Fixed& other) const{
+	return this->value < other.value ? true : false;
+}
+
+bool Fixed::operator>=(const Fixed& other) const{
+	return this->value >= other.value ? true : false;
+}
+
+bool Fixed::operator<=(const Fixed& other) const{
+	return this->value <=  other.value ? true : false;
+}
+
+bool Fixed::operator==(const Fixed& other) const{
+	return this->value == other.value ? true : false;
+}
+
+bool Fixed::operator!=(const Fixed& other) const{
+	return this->value != other.value ? true : false;
+}
+
+Fixed Fixed::operator+(const Fixed& other) const{
+	return this->toFloat() + other.toFloat();
+}
+
+Fixed Fixed::operator-(const Fixed& other) const{
+	return this->toFloat() - other.toFloat();
+} 
+
+Fixed Fixed::operator*(const Fixed& other) const{
+	return this->toFloat() * other.toFloat();
+}
+
+Fixed Fixed::operator/(const Fixed& other) const{
+	return this->toFloat() / other.toFloat();
+}
+
+void Fixed::operator+=(const Fixed& other)
+{
+	*this = *this + other;
+}
+
+void Fixed::operator-=(const Fixed& other)
+{
+	*this = *this - other;
+}
+
+void Fixed::operator*=(const Fixed& other)
+{
+	*this = *this * other;
+}
+
+void Fixed::operator/=(const Fixed& other){
+	*this = *this / other;
+}
+
+Fixed& Fixed::operator++(void){
+	this->value++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int){
+	Fixed tmp(*this);
+	operator++();
+	return (tmp);
+}
+
+Fixed& Fixed::operator--(void){
+	this->value--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int){
+	Fixed tmp(*this);
+	operator--();
+	return tmp;
+}
+
+Fixed& Fixed::min(Fixed& a, Fixed& b){
+	return a < b ? a : b;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b){
+	return a > b ? a : b;
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b){
+	return a < b ? a : b;
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b){
+	return a > b ? a : b;
+}
