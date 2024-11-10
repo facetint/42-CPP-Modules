87f5c53f5b6615c52a62e53384ac42e814bd629a
facetint <fatmanurcetintas7@gmail.com>
2024-11-10 23:20:44 +0300

    refactor: update DiamondTrap constructors and operator= for clarity and consistency

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -12,23 +12,25 @@
 
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap() : ClapTrap(""), ScavTrap(""), FragTrap("")
+DiamondTrap::DiamondTrap() : ClapTrap()
 {
 	std::cout << "Default constructor called - (DiamondTrap)" << std::endl;
-	setDiamondName("");
+	this->_name = "DiamondTrap";
+	this->_hitPoint = FragTrap::_hitPoint;
+	this->_energyPoint = ScavTrap::_energyPoint;
+	this->_attackDamage = FragTrap::_attackDamage;
 }
 
 DiamondTrap::DiamondTrap(const std::string &name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name)
 {
 	std::cout << "Parameterized constructor called - (DiamondTrap)" << std::endl;
-	setDiamondName(name);
-	setHitPoint(100);
-	setEnergyPoint(50);
-	setAttackDamage(30);
+	this->_name = name;
+	this->_hitPoint = 100;
+	this->_energyPoint = 50;
+	this->_attackDamage = 30;
 }
 
-DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other),
-	ScavTrap(other), FragTrap(other)
+DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other), ScavTrap(other), FragTrap(other)
 {
 	std::cout << "Copy constructor called - (DiamondTrap)" << std::endl;
 	*this = other;
@@ -44,8 +46,10 @@ DiamondTrap &DiamondTrap::operator=(const DiamondTrap &other)
 	std::cout << "Copy assignment operator called - (DiamondTrap)" << std::endl;
 	if (this != &other)
 	{
-		this->_name = other.getDiamondName();
-		this->ClapTrap::operator=(other);
+		this->_name = other._name;
+		this->_hitPoint = other._hitPoint;
+		this->_energyPoint = other._energyPoint;
+		this->_attackDamage = other._attackDamage;
 	}
 	return (*this);
 }
