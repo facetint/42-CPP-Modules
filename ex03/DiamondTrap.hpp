08de4e1a88e5e8310d26883ab270a27c52727079
facetint <fatmanurcetintas7@gmail.com>
2024-11-10 23:21:55 +0300

    refactor: add public default constructor and remove unused iostream include in DiamondTrap

--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -1,29 +1,27 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: facetint <facetint@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fatmanurcetintas <fatmanurcetintas@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:01:29 by facetint          #+#    #+#             */
-/*   Updated: 2024/10/30 11:01:30 by facetint         ###   ########.fr       */
+/*   Updated: 2024/11/10 23:21:07 by fatmanurcet      ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef DIAMONDTRAP_HPP
 # define DIAMONDTRAP_HPP
 
 # include "FragTrap.hpp"
 # include "ScavTrap.hpp"
-# include <iostream>
 
 class DiamondTrap : public ScavTrap, public FragTrap
 {
   private:
 	std::string _name;
-	DiamondTrap();
-
   public:
+	DiamondTrap();
 	DiamondTrap(const std::string &name);
 	DiamondTrap(const DiamondTrap &other);
 	~DiamondTrap();
