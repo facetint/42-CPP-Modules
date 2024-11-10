c5387d0563a3106b8bfe17753d012896e528f71f
Fatmanur Cetintas <facetint@k1m17s02.42kocaeli.com.tr>
2024-11-10 17:06:30 +0300

    feat: add try-catch block for memory allocation error handling in main function

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,26 +6,35 @@
 #include "Cure.hpp"
 #include "ICharacter.hpp"
 
-
-int main()
+ int main()
 {
-    IMateriaSource* src = new MateriaSource();
-    src->learnMateria(new Ice());
-    src->learnMateria(new Cure());
-    ICharacter* me = new Character("me");
-    AMateria* tmp;
-    tmp = src->createMateria("ice");
-    me->equip(tmp);
-    tmp = src->createMateria("cure");
-    me->equip(tmp);
-    ICharacter* bob = new Character("bob");
+    try
+    {
+        IMateriaSource* src = new MateriaSource();
+        src->learnMateria(new Ice());
+        src->learnMateria(new Cure());
 
-    me->use(0, *bob);
-    me->use(1, *bob);
+        ICharacter* me = new Character("me");
 
-    delete bob;
-    delete me;
-    delete src;
+        AMateria* tmp;
+        tmp = src->createMateria("ice");
+        me->equip(tmp);
+        tmp = src->createMateria("cure");
+        me->equip(tmp);
 
-return 0;
+        ICharacter* bob = new Character("bob");
+
+        me->use(0, *bob);
+        me->use(1, *bob);
+
+        delete bob;
+        delete me;
+        delete src;
+    }
+    catch (const std::bad_alloc& e)
+    {
+        std::cerr << "Memory allocation failed: " << e.what() << std::endl;
+    }
+    return 0;
 }
+ 
