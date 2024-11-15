3fb897247864a4246ece8ac86085dc3a12d76916
Fatmanur Cetintas <facetint@k1m15s06.42kocaeli.com.tr>
2024-11-15 18:58:42 +0300

    feat: implement Bureaucrat class methods with grade validation

---
+++ ex00/ Bureaucrat.cpp
@@ -0,0 +1,48 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150) {}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade) : _name(name)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    _grade = grade;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &other) : _name(other._name), _grade(other._grade) {}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &other)
+{
+    if (this == &other)
+        return *this;
+    _grade = other._grade;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+std::string const &Bureaucrat::getName() const
+{
+    return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return _grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+    if (_grade == 1)
+        throw Bureaucrat::GradeTooHighException();
+    _grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    if (_grade == 150)
+        throw Bureaucrat::GradeTooLowException();
+    _grade++;
+}
