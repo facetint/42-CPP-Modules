482a32b3362d6f35e309f44a2e6d583a9a24b7a2
Fatmanur Cetintas <facetint@k1m15s06.42kocaeli.com.tr>
2024-11-15 18:48:01 +0300

    feat: add Bureaucrat class with basic attributes and member functions

---
+++ ex00/ Bureaucrat.hpp
@@ -0,0 +1,21 @@
+#pragma one
+
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+    std::string const _name;
+    int _grade;
+public:
+    Bureaucrat();
+    Bureaucrat(std::string const name, int grade);
+    Bureaucrat(Bureaucrat const &other);
+    Bureaucrat &operator=(Bureaucrat const &other);
+    ~Bureaucrat();
+    std::string const &getName() const;
+    int getGrade() const;
+    void incrementGrade() const;
+    void decrementGrade() const;
+    
+};
