96bf84bb8b2f8fa8fb54724d5d4db2095fa51cfd
facetint <fatmanurcetintas7@gmail.com>
2025-01-02 02:02:17 +0300

    chore: add the same Bureaucrat files from exercise ex01

---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+
+# define RESET "\x1b[0m"
+# define BLUE "\x1b[34;01m"
+
+# define GRADE_MIN 1
+# define GRADE_MAX 150
+
+class Bureaucrat
+{
+private:
+    std::string const _name;
+    int _grade;
+public:
+    Bureaucrat();
+    Bureaucrat(std::string const name, int grade);
+    Bureaucrat(Bureaucrat const &other);
+    Bureaucrat &operator=(Bureaucrat const &other);
+    ~Bureaucrat();
+    std::string const &getName() const;
+    int getGrade() const;
+    void setName(std::string const name);
+    void incrementGrade();
+    void decrementGrade();
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw();
+    };
+
+};
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat);
