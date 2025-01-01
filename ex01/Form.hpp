612225dfcf67f71b6f5a833bc6faaaf6d516ff7c
facetint <fatmanurcetintas7@gmail.com>
2025-01-02 01:54:04 +0300

    refactor: add color macros to the Form class

--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -3,6 +3,12 @@
 #include <iostream>
 #include "Bureaucrat.hpp"
 
+# define RED "\x1b[31;01m"
+# define RESET "\x1b[0m"
+# define GREEN "\x1b[32;01m"
+# define WHITE "\x1b[38;2;255;255;255;01m"
+# define PURPLE "\x1b[38;2;178;58;238;01m"
+
 class Form
 {
 private:
