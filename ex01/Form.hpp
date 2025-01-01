12dd869192b32ceed8abb3ddf55d666da0cf1aff
facetint <fatmanurcetintas7@gmail.com>
2025-01-02 00:37:40 +0300

    feat: Add Form class with attributes, methods, and exception handling

---
+++ ex01/Form.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+class Form
+{
+private:
+    std::string const _name;
+    bool _isSigned;
+    const int _signGrade;
+    const int _executeGrade;
+
+public:
+    Form();
+    Form(std::string const name, int gradeToSign, int gradeToExecute);
+    Form(Form const &other);
+    Form &operator=(Form const &other);
+    ~Form();
+
+    std::string const &getName() const;
+    int getGradeToSign() const;
+    int getGradeToExecute() const;
+    bool getSigned() const;
+
+    void setName(std::string const name);
+    void setSign(bool sign);
+    void setGradeToSign(int gradeToSign);
+    void setGradeToExecute(int gradeToExecute);
+
+    void beSigned(const Bureaucrat &bureaucrat);
+
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw();
+    };
+
+};
+
+std::ostream &operator<<(std::ostream &out, Form const &form);
+
