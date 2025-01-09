ba4b278be48a3fc394de9e8eb2493b26c28f3ab7
Fatmanur Cetintas <facetint@k2m05s03.42kocaeli.com.tr>
2025-01-09 17:17:49 +0300

    feat: implement ShrubberyCreationForm constructor and destructor

---
+++ ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,32 @@
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137), _target("default")
+{
+    std::cout << "Default constructor called - (ShrubberyCreationForm)" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string target) : AForm("ShrubberyCreationForm", 145, 137), _target(target)
+{
+    std::cout << "Parametric constructor called - (ShrubberyCreationForm)" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &other) : AForm(other), _target(other._target)
+{
+    std::cout << "Copy constructor called - (ShrubberyCreationForm)" << std::endl;
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(ShrubberyCreationForm const &other)
+{
+    std::cout << "Assignation operator called - (ShrubberyCreationForm)" << std::endl;
+    if (this != &other)
+    {
+        AForm::operator=(other);
+        setTarget(other.getTarget());
+    }
+    return *this;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+    std::cout << "Destructor called - (ShrubberyCreationForm)" << std::endl;
+}
