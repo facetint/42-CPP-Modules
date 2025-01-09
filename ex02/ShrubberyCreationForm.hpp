1f89359226e9bf01799987aa07b938a684d6b6bd
Fatmanur Cetintas <facetint@k2m05s03.42kocaeli.com.tr>
2025-01-09 16:41:45 +0300

    feat: implement ShrubberyCreationForm class

---
+++ ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+# include "Bureaucrat.hpp"
+# include <fstream>
+
+class ShrubberyCreationForm : public AForm
+{
+private:
+    const std::string _target;
+
+public:
+    ShrubberyCreationForm();
+    ShrubberyCreationForm(std::string target);
+    ShrubberyCreationForm(ShrubberyCreationForm const &other);
+    ShrubberyCreationForm &operator=(ShrubberyCreationForm const &other);
+    virtual ~ShrubberyCreationForm();
+
+    std::string const &getTarget() const;
+    void setTarget(std::string target);
+
+    void execute(Bureaucrat const &executor) const;
+
+};
