3e7ed71ad5a2be2a44a98f3b50a057a0e34de597
Fatmanur Cetintas <facetint@k2m05s03.42kocaeli.com.tr>
2025-01-09 17:05:06 +0300

    chore: add ANSI color macros for better console outputs

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -2,8 +2,11 @@
 
 #include <iostream>
 
-#define GRADE_MIN 1
-#define GRADE_MAX 150
+# define RESET "\x1b[0m"
+# define BLUE "\x1b[34;01m"
+
+# define GRADE_MIN 1
+# define GRADE_MAX 150
 
 class Bureaucrat
 {
