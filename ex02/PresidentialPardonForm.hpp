a03a03f1d04e32ff42d37d3c64bbda23d0b6768c
facetint <fatmanurcetintas7@gmail.com>
2025-01-13 16:35:50 +0300

    feat: add  PresidentialPardonForm   class with _target member, constructors, getters, setters, and an execute method

---
+++ ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+# include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+private:
+    const std::string _target;
+
+public:
+    PresidentialPardonForm();
+    PresidentialPardonForm(const std::string target);
+    PresidentialPardonForm(PresidentialPardonForm const &other);
+    PresidentialPardonForm &operator=(PresidentialPardonForm const &other);
+    virtual ~PresidentialPardonForm();
+
+    std::string const &getTarget() const;
+    void setTarget(std::string target);
+
+    void execute(Bureaucrat const &executor) const;
+};
