e2b116d150e0ea391bd4c7b09a3891a3d3a5a5bc
facetint <fatmanurcetintas7@gmail.com>
2025-01-15 02:25:31 +0300

    feat:  add default constructor, copy constructor, copy assignment operator, and destructor

---
+++ ex03/Intern.cpp
@@ -0,0 +1,29 @@
+#include "Intern.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+Intern::Intern()
+{
+    std::cout << "Default constructor called - (Intern)" << std::endl;
+}
+
+Intern::Intern(Intern const &other)
+{
+    std::cout << "Copy constructor called - (Intern)" << std::endl;
+    *this = other;
+}
+
+Intern &Intern::operator=(Intern const &other)
+{
+    std::cout << "Assignation operator called - (Intern)" << std::endl;
+    (void)other;
+    return *this;
+}
+
+Intern::~Intern()
+{
+    std::cout << "Destructor called - (Intern)" << std::endl;
+}
+
+
