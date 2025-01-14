675e6295187ca2489791b5d10972a0d598aaf7fb
facetint <fatmanurcetintas7@gmail.com>
2025-01-15 02:28:13 +0300

    feat: add the makeForm method to the Intern class to dynamically create forms based on the input formName

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -31,4 +31,23 @@ const char *Intern::FormNotFound::what() const throw()
     return "Form not found";
 }
 
+AForm *Intern::makeForm(std::string const &formName, std::string const &target)
+{
 
+    const std::string formNames[] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+    AForm* (*formCreators[])(const std::string&) = {
+        [](const std::string& target) { return static_cast<AForm*>(new ShrubberyCreationForm(target)); },
+
+        [](const std::string& target) { return static_cast<AForm*>(new RobotomyRequestForm(target)); },
+
+        [](const std::string& target) { return static_cast<AForm*>(new PresidentialPardonForm(target)); }
+    };
+    const size_t formCount = sizeof(formNames) / sizeof(formNames[0]);
+    for (size_t i = 0; i < formCount; ++i) {
+        if (formName == formNames[i]) {
+            std::cout << "Intern creates " << formName << " form" << std::endl;
+            return formCreators[i](target);
+        }
+    }
+    throw FormNotFound();
+}
