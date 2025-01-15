2864c97c1c26b8a2ce1c199a1667a7b9da39f673
facetint <fatmanurcetintas7@gmail.com>
2025-01-15 21:39:09 +0300

    refactor: improve Bureaucrat main function to focus on individual cases

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,46 +5,42 @@ int main()
     try
     {
         Bureaucrat *bureaucrat1 = new Bureaucrat("facetint", 10);
-        Bureaucrat *bureaucrat2 = new Bureaucrat("fatmanur", 140);
 
         std::cout << *bureaucrat1 << std::endl;
-        std::cout << *bureaucrat2 << std::endl;
 
         bureaucrat1->incrementGrade();
         std::cout << "After increment: " << *bureaucrat1 << std::endl;
 
-        bureaucrat2->decrementGrade();
-        std::cout << "After decrement: " << *bureaucrat2 << std::endl;
-
-        for (int i = 0; i < 10; ++i)
+        for (size_t i = 0; i < 10; ++i)
         {
             bureaucrat1->incrementGrade();
         }
+        
         delete bureaucrat1;
-        delete bureaucrat2;
     }
     catch (const Bureaucrat::GradeTooHighException &e)
     {
         std::cerr << "GradeTooHighException caught: " << e.what() << std::endl;
     }
-    catch (const Bureaucrat::GradeTooLowException &e)
-    {
-        std::cerr << "GradeTooLowException caught: " << e.what() << std::endl;
-    }
-
     try
     {
-        Bureaucrat *invalidBureaucrat = new Bureaucrat("Invalid", 200);
-        delete invalidBureaucrat;
-    }
-    catch (const Bureaucrat::GradeTooHighException &e)
-    {
-        std::cerr << "GradeTooHighException caught: " << e.what() << std::endl;
+        Bureaucrat *bureaucrat2 = new Bureaucrat("fatmanur", 145);
+
+        std::cout << *bureaucrat2 << std::endl;
+
+        bureaucrat2->decrementGrade();
+        std::cout << "After decrement: " << *bureaucrat2 << std::endl;
+
+        for (size_t i = 0; i < 5; ++i)
+        {
+            bureaucrat2->decrementGrade();
+        }
+
+        delete bureaucrat2;
     }
     catch (const Bureaucrat::GradeTooLowException &e)
     {
         std::cerr << "GradeTooLowException caught: " << e.what() << std::endl;
     }
-
     return 0;
 }
