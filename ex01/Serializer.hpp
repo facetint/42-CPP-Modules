0fcd06b141de21ed10494c68581dbcb126189798
facetint <fatmanurcetintas7@gmail.com>
2025-01-20 22:00:05 +0300

    fix: fix class name

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <stdint.h>
+
+struct Data;
+
+class Serializer
+{
+    private:
+        Serializer();
+        Serializer(Serializer const &src);
+        ~Serializer();
+        Serializer & operator=(Serializer const &other);
+    public:
+        static uintptr_t serialize(Data *ptr);
+        static Data *deserialize(uintptr_t raw);
+};
