8d45b8025b73bc9b9eb6f0d575c4aad81facdf3a
facetint <fatmanurcetintas7@gmail.com>
2025-01-20 21:42:14 +0300

    feat: implement serialization and deserialization methods

--- ex01/Serialization.cpp
+++ ex01/Serialization.cpp
@@ -23,3 +23,12 @@ Serialization & Serialization::operator=(Serialization const & other)
     return *this;
 }
 
+uintptr_t Serialization::serialize(Data *ptr)
+{
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *Serialization::deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data *>(raw);
+}
