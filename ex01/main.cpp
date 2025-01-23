994d5450102a828006e9f3778e528d7f91543d0e
facetint <fatmanurcetintas7@gmail.com>
2025-01-23 18:35:35 +0300

    style: add red color formatting to section titles in main using RED and RESET macros

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,13 +7,13 @@ int main()
     float arr3[] = {1.1, 2.2, 3.3, 4.4, 5.5};
     double arr4[] = {1.1, 2.2, 3.3, 4.4, 5.5};
 
-    std::cout << " **** int array ****" << std::endl;
+    std::cout << RED <<  " **** int array ****" << RESET << std::endl;
     iter(arr, 3, print);
-    std::cout << " **** string array ****" << std::endl;
+    std::cout << RED << "\n **** string array ****" << RESET << std::endl;
     iter(arr2, 3, print);
-    std::cout << " **** float array **** " << std::endl;
+    std::cout << RED << "\n **** float array **** " << RESET << std::endl;
     iter(arr3, 5, print);
-    std::cout << " **** double array ****" << std::endl;
+    std::cout << RED << "\n **** double array ****" << RESET << std::endl;
     iter(arr4, 5, print);
 
     return (0);
