ccb2d45133b0a6a3017287fb7b7e82a28252e79b
facetint <fatmanurcetintas7@gmail.com>
2025-01-23 18:14:01 +0300

    feat: add Array class template with constructor, copy constructor, assignment operator, and destructor

---
+++ ex02/Array.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+template <typename T>
+
+class Array
+{
+    private:
+        T *_arr;
+        unsigned int _size;
+
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(Array const &other);
+        ~Array();
+        Array &operator=(Array const &other);
+        T const &operator[](unsigned int i) const;
+        T &operator[](unsigned int i);
+        unsigned int size() const;
+
+};
+
+#include "Array.tpp"
