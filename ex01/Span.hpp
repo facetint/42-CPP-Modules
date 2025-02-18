e48a28cadbd03a9d532af6b136b2bca6953e6033
facetint <fatmanurcetintas7@gmail.com>
2025-02-18 22:34:06 +0300

    feat: Introduced a new method 'addNumbersRange' that allows adding a range of numbers to the Span object

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -22,4 +22,15 @@ class Span
         void addNumber(int number);
         int shortestSpan();
         int longestSpan();
+
+        template <typename Iter>
+        void addNumbersRange(Iter begin, Iter end)
+        {
+            size_t distance = std::distance(begin, end);
+            if (_numbers.size() + distance > _maxSize)
+            {
+                throw std::overflow_error("Span cannot accommodate all numbers in the range.");
+            }
+            _numbers.insert(_numbers.end(), begin, end);
+        }
 };
