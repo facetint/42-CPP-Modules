6c6a02014c6f0999f395fcc87a731b77588a5490
facetint <fatmanurcetintas7@gmail.com>
2025-02-18 21:28:47 +0300

    feat: add easyfind function and NotFoundException class for container search

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <algorithm>
+#include <exception>
+#include <iostream>
+#include <iterator>
+
+# define RED "\x1b[31;01m"
+# define RESET "\x1b[0m"
+# define GREEN "\x1b[32;01m"
+# define PURPLE "\x1b[38;2;178;58;238;01m"
+# define WHITE "\x1b[37;01m"
+
+class NotFoundException : public std::exception
+{
+public:
+    virtual const char *what() const throw();
+};
+
+template <typename T>
+int easyfind(T &container, int value);
+
+template <typename T>
+void testEasyFind(T &container, int value);
+
+#include "easyfind.tpp"
