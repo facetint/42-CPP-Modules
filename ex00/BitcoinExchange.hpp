c8af50b3be62590a839e914bef768f706e5bd4ca
facetint <fatmanurcetintas7@gmail.com>
2025-02-21 03:26:37 +0300

    feat: introduce exception handling for invalid data and file operations

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -29,5 +29,29 @@ class BitcoinExchange
         bool loadDatabase(const std::string& filename);
         void processInputFile(const std::string& filename);
         void processArgsAndFiles(int ac, char **av);
+    
+        class InvalidDateException : public std::exception 
+        {
+        public:
+            const char* what() const throw() { return "Error: Invalid date format."; }
+        };
+
+        class InvalidValueException : public std::exception 
+        {
+        public:
+            const char* what() const throw() { return "Error: Value must be between 0 and 1000."; }
+        };
+
+        class FileOpenException : public std::exception
+        {
+        public:
+            const char* what() const throw() { return "Error: Could not open file."; }
+        };
+
+        class BadInputFormatException : public std::exception
+        {
+        public:
+            const char* what() const throw() { return "Error: Bad input format."; }
+        };
 
 };
