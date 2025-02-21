850d35953cefa981e8a2e529f8710fcd2b866dae
facetint <fatmanurcetintas7@gmail.com>
2025-02-21 05:41:34 +0300

    feat: Add RPN calculator class with operator validation and exception handling

---
+++ ex01/RPN.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <exception>
+
+
+class RPN
+{
+    public:
+    static bool is_operator(const std::string& s);
+    static int perform_operation(int a, int b, const std::string& op);
+    static int evaluate(const std::string& expression);
+
+    class InvalidOperatorException : public std::exception {
+        public:
+            const char* what() const throw() {
+                return "Error: Invalid operator.";
+            }
+    };
+
+    class InvalidTokenException : public std::exception {
+        public:
+            const char* what() const throw(){
+                return "Error: Invalid token (must be a number or operator).";
+            }
+    };
+
+    class DivisionByZeroException : public std::exception {
+        public:
+            const char* what() const throw(){
+                return "Error: Division by zero.";
+            }
+    };
+
+    class InvalidExpressionException : public std::exception {
+        public:
+            const char* what() const throw() {
+                return "Error: Invalid expression.";
+            }
+    };
+};
