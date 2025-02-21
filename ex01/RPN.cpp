d525cacfad1c4482c304a3d268f699246d057d58
facetint <fatmanurcetintas7@gmail.com>
2025-02-21 05:42:18 +0300

    feat: Implement RPN evaluation with operator handling and exception management

---
+++ ex01/RPN.cpp
@@ -0,0 +1,64 @@
+#include "RPN.hpp"
+
+bool RPN::is_operator(const std::string& s)
+{
+    return s == "+" || s == "-" || s == "*" || s == "/";
+}
+
+int RPN::perform_operation(int a, int b, const std::string& op) 
+{
+    if (op == "+")
+        return a + b;
+    if (op == "-")
+        return a - b;
+    if (op == "*")
+        return a * b;
+    if (op == "/") {
+        if (b == 0) {
+            throw DivisionByZeroException();
+        }
+        return a / b;
+    }
+    throw InvalidOperatorException();
+}
+
+
+int RPN::evaluate(const std::string& expression)
+{
+    std::stringstream ss(expression); 
+    std::string token;
+    std::stack<int> stack;
+
+    while (ss >> token)
+    {
+        if (is_operator(token))
+        {
+            if (stack.size() < 2) {
+                throw InvalidExpressionException();
+            }
+            int b = stack.top();
+            stack.pop();
+            int a = stack.top();
+            stack.pop();
+            int result = perform_operation(a, b, token);
+            stack.push(result);
+        }
+        else
+        {
+            try {
+                if (token.size() != 1 || !std::isdigit(token[0])) {
+                    throw InvalidTokenException();
+                }
+                int num = token[0] - '0';
+                stack.push(num);
+            } catch (const std::invalid_argument& e) {
+                throw InvalidTokenException();
+            }
+        }
+    }
+    if (stack.size() != 1) {
+        throw InvalidExpressionException();
+    }
+
+    return stack.top();
+}
