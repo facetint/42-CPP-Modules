a441688606f0fce4f1edce08bdf5abf79a5491ab
facetint <fatmanurcetintas7@gmail.com>
2025-02-23 04:22:14 +0300

    feat: add constructors, assignment operator, and destructor for PmergeMe class

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,21 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& other)
+{
+    *this = other;
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+    if (this != &other)
+    {
+        _vec = other._vec;
+        _deq = other._deq;
+    }
+    return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
