0abb60db6f2835ecbe7c282ea25b9acceae1bda8
facetint <fatmanurcetintas7@gmail.com>
2025-04-21 02:12:55 +0300

    refactor: use Vector and Deque subclasses for cleaner processing and output

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,20 +4,21 @@ int main(int ac, char** av)
 { 
     try
     {
-        FordJhonson<std::vector<int>, std::vector<std::pair<int, int > > > vector;
-        vector.process(ac, av);
-
-        FordJhonson<std::deque<int>, std::deque<std::pair<int, int > > > deque;
-        deque.process(ac, av);
+        FordJohnson<std::vector<int>, std::vector<std::pair<int, int > > > vector = Vector(ac, av);
+        vector.fordJohnson();
+        
+        FordJohnson<std::deque<int>, std::deque<std::pair<int, int > > > deque = Deque(ac, av);
+        deque.fordJohnson();
 
         vector.printBefore();
         vector.printAfter();
+        vector.printTime(ac - 1);
+        deque.printTime(ac - 1);
 
     } catch (const std::exception& e)
     {
-        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
